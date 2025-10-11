8989ee9ae033c238e3de5a027f3878e7fa96bb93
6298287 <6298287@cstj.qc.ca>
2025-10-11 17:03:12 -0400

    Library V2

--- Trick_or_Trade/Assets/Scripts/GenerationCarte.cs
+++ Trick_or_Trade/Assets/Scripts/GenerationCarte.cs
@@ -6,17 +6,17 @@ public class GenerationCarte : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected string nom_Carte = "Carte Inconnue";
     protected string description_Carte = "Aucune description";
-
     public virtual void CreerLaCarte() { }
     protected void afficher_carte()
     {
-        GameObject[] cartes = GameObject.FindGameObjectsWithTag("Carte");
+        //GameObject[] cartes = GameObject.FindGameObjectsWithTag("Carte");
 
-        foreach (GameObject carte in cartes)
-        {
-            TextMeshPro tmp = carte.GetComponent<TextMeshPro>();
-            tmp.text = "name : " + nom_Carte + "\n" + "\n" + "description : " + description_Carte;
+        Debug.Log("Ce script est attaché à : " + gameObject.name);
 
-        }
+
+
+        //foreach (GameObject carte in cartes)
+        TextMeshPro tmp = gameObject.GetComponent<TextMeshPro>();
+        tmp.text = "name : " + nom_Carte + "\n" + "\n" + "description : " + description_Carte;
     }
 }
--- Trick_or_Trade/Assets/Scripts/Library_Card/Recycle.cs
+++ Trick_or_Trade/Assets/Scripts/Library_Card/Recycle.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class CarteDeJeu : GenerationCarte
 {
+
     override public void CreerLaCarte()
     {
+        Debug.Log("I'm Working");
         nom_Carte = "Recycle";
         description_Carte = "Put a card from any discard pile into your hand.";
+        afficher_carte();
     }
 }
