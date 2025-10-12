2a63874104ccece2f0a497bee9bd45c3006ecebf
goumleur <goumleur@gmail.com>
2025-10-12 08:51:05 -0400

    bug regler affichagee carte

--- Trick_or_Trade/Assets/Scripts/GenerationCarte.cs
+++ Trick_or_Trade/Assets/Scripts/GenerationCarte.cs
@@ -7,13 +7,10 @@ public class GenerationCarte : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
    [SerializeField] protected string nom_Carte = "Carte Inconnue";
    [SerializeField] protected string description_Carte = "Aucune description";
-   [SerializeField] protected Sprite image_Carte = null;
-
     public virtual void CreerLaCarte() { }
    [SerializeField] protected void afficher_carte()
     {
-        Image img = gameObject.transform.GetChild(1).GetComponent<Image>();
-        img.sprite = image_Carte;
+
         Debug.Log("Ce script est attaché à : " + gameObject.name);
         TextMeshPro tmp = gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshPro>();
         tmp.text = "name : " + nom_Carte + "\n" + "\n";
--- Trick_or_Trade/Assets/Scripts/Library_Card/Recycle.cs
+++ Trick_or_Trade/Assets/Scripts/Library_Card/Recycle.cs
@@ -10,7 +10,6 @@ public class CarteDeJeu : GenerationCarte
     override public void CreerLaCarte()
     {
         Debug.Log("I'm Working");
-        image_Carte = Resources.Load<Sprite>("Images/ImageRecycle");
         nom_Carte = "Recycle";
         description_Carte = "Put a card from any discard pile into your hand.";
         afficher_carte();
