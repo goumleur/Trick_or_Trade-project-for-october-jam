5c1d271d5bfcf209fdc20aac36d2efaa11518930
6298287 <6298287@cstj.qc.ca>
2025-10-12 15:46:44 -0400

    Setting V1

---
+++ Trick_or_Trade/Assets/Scripts/Menu/Settings.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    // Section video
+    bool fullScreen = false;
+    string resolution = "HD";
+
+    // Full Screen
+    public void FullScreenSet()
+    {
+        if (fullScreen == false)
+        {
+            fullScreen = true;
+        }
+        else if (fullScreen == true)
+        {
+            fullScreen = false;
+        }
+        else
+        {
+            Debug.LogError("Error, the full screen button has an error");
+            return;
+        }
+        Screen.fullScreen = fullScreen;
+        Debug.Log($"Full screen {fullScreen}");
+    }
+
+    // Résolution
+    public void ResolutionSetDroite()
+    {
+        if (resolution == "HD") // Set en full hd
+        {
+            Screen.SetResolution(1920, 1080, fullScreen);
+            resolution = "FullHD";
+        }
+        else if (resolution == "FullHD")
+        {
+            Screen.SetResolution(2560, 1440, fullScreen);
+            resolution = "2K";
+        }
+        else if (resolution == "2K")
+        {
+            Screen.SetResolution(3840, 2160, fullScreen);
+            resolution = "4K";
+        }
+        else if (resolution == "4K")
+        {
+            Screen.SetResolution(3440, 1440, fullScreen);
+            resolution = "UltraWide";
+        }
+        else if (resolution == "UltraWide")
+        {
+            Screen.SetResolution(1280, 720, fullScreen);
+        }
+        else
+        {
+            Debug.LogError("Error, the Resolution Button has an error");
+            return;
+        }
+        Debug.Log($"The resolution is : {resolution}");
+        Debug.Log($"Weidth = {Screen.width}");
+        Debug.Log($"Height = {Screen.height}");
+        Debug.Log($"Full Screen = {Screen.fullScreen}");
+    }
+}
