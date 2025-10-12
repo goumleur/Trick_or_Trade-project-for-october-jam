120678da3acbdc01be429f0d4f2b54fd35e41c8c
goumleur <goumleur@gmail.com>
2025-10-12 12:12:21 -0400

    menu 75%

---
+++ Trick_or_Trade/Assets/Scripts/leaving_Script.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+
+public class leaving_Script : MonoBehaviour
+{
+   public void quit()
+    {
+        Application.Quit();
+        Debug.Log("you leave successfully");
+    }
+}
